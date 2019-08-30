b0058021122a44d0d760c99ddf3d43eeaa753e51
benjamin dubreu <47891686+bdubreu-adeo@users.noreply.github.com>
2019-08-30 17:21:31 +0200

    Update layers.py

--- fastai/layers.py
+++ fastai/layers.py
@@ -1,11 +1,14 @@
 "`fastai.layers` provides essential functions to building and modifying `model` architectures"
 from .torch_core import *
 
-__all__ = ['AdaptiveConcatPool2d', 'BCEWithLogitsFlat', 'BCEFlat', 'MSELossFlat', 'CrossEntropyFlat', 'Debugger',
-           'Flatten', 'Lambda', 'PoolFlatten', 'View', 'ResizeBatch', 'bn_drop_lin', 'conv2d', 'conv2d_trans', 'conv_layer',
-           'embedding', 'simple_cnn', 'NormType', 'relu', 'batchnorm_2d', 'trunc_normal_', 'PixelShuffle_ICNR', 'icnr',
-           'NoopLoss', 'WassersteinLoss', 'SelfAttention', 'SequentialEx', 'MergeLayer', 'res_block', 'sigmoid_range',
-           'SigmoidRange', 'PartialLayer', 'FlattenedLoss', 'BatchNorm1dFlat', 'LabelSmoothingCrossEntropy', 'PooledSelfAttention2d']
+__all__ = ['AdaptiveConcatPool2d', 'BCEWithLogitsFlat', 'BCEFlat', 'MSELossFlat', 
+           'CrossEntropyFlat', 'Debugger', 'Flatten', 'Lambda', 'PoolFlatten', 'View', 
+           'ResizeBatch', 'bn_drop_lin', 'conv2d', 'conv2d_trans', 'conv_layer', 'embedding', 
+           'simple_cnn', 'NormType', 'relu', 'batchnorm_2d', 'trunc_normal_', 
+           'PixelShuffle_ICNR', 'icnr', 'NoopLoss', 'WassersteinLoss', 'SelfAttention', 
+           'SequentialEx', 'MergeLayer', 'res_block', 'sigmoid_range', 'SigmoidRange', 
+           'PartialLayer', 'FlattenedLoss', 'BatchNorm1dFlat', 'LabelSmoothingCrossEntropy', 
+           'PooledSelfAttention2d']
 
 class Lambda(Module):
     "Create a layer that simply calls `func` with `x`"
@@ -23,7 +26,8 @@ class ResizeBatch(Module):
     def forward(self, x): return x.view((x.size(0),) + self.size)
 
 class Flatten(Module):
-    "Flatten `x` to a single dimension, often used at the end of a model. `full` for rank-1 tensor"
+    """Flatten `x` to a single dimension, often used at the end of a model. `full` 
+           for rank-1 tensor"""
     def __init__(self, full:bool=False): self.full = full
     def forward(self, x): return x.view(-1) if self.full else x.view(x.size(0), -1)
 
@@ -42,7 +46,9 @@ def batchnorm_2d(nf:int, norm_type:NormType=NormType.Batch):
     return bn
 
 def bn_drop_lin(n_in:int, n_out:int, bn:bool=True, p:float=0., actn:Optional[nn.Module]=None):
-    "Sequence of batchnorm (if `bn`), dropout (with `p`) and linear (`n_in`,`n_out`) layers followed by `actn`."
+           
+    """Sequence of batchnorm (if `bn`), dropout (with `p`) and linear (`n_in`,`n_out`) layers
+    followed by `actn`."""
     layers = [nn.BatchNorm1d(n_in)] if bn else []
     if p != 0: layers.append(nn.Dropout(p))
     layers.append(nn.Linear(n_in, n_out))
@@ -65,7 +71,7 @@ class PooledSelfAttention2d(Module):
         self.g     = spectral_norm(conv2d(n_channels, n_channels//2, 1)) # value
         self.o     = spectral_norm(conv2d(n_channels//2, n_channels, 1))
         self.gamma = nn.Parameter(tensor([0.]))
-
+        self.shuf = PixelShuffle_ICNR(up_in_c, up_in_c//2, blur=blur, leaky=leaky, **kwargs)
     def forward(self, x):
         # code borrowed from https://github.com/ajbrock/BigGAN-PyTorch/blob/7b65e82d058bfe035fc4e299f322a1f83993e04c/layers.py#L156
         theta = self.theta(x)
@@ -75,7 +81,8 @@ class PooledSelfAttention2d(Module):
         phi = phi.view(-1, self.n_channels // 8, x.shape[2] * x.shape[3] // 4)
         g = g.view(-1, self.n_channels // 2, x.shape[2] * x.shape[3] // 4)
         beta = F.softmax(torch.bmm(theta.transpose(1, 2), phi), -1)
-        o = self.o(torch.bmm(g, beta.transpose(1,2)).view(-1, self.n_channels // 2, x.shape[2], x.shape[3]))
+        o = self.o(torch.bmm(g, beta.transpose(1,2)).view(
+                   -1, self.n_channels // 2, x.shape[2], x.shape[3]))
         return self.gamma * o + x
 
 class SelfAttention(Module):
@@ -95,28 +102,35 @@ class SelfAttention(Module):
         o = self.gamma * torch.bmm(h, beta) + x
         return o.view(*size).contiguous()
 
-def conv2d(ni:int, nf:int, ks:int=3, stride:int=1, padding:int=None, bias=False, init:LayerFunc=nn.init.kaiming_normal_) -> nn.Conv2d:
+def conv2d(ni:int, nf:int, ks:int=3, stride:int=1, padding:int=None, bias=False, 
+           init:LayerFunc=nn.init.kaiming_normal_) -> nn.Conv2d:
     "Create and initialize `nn.Conv2d` layer. `padding` defaults to `ks//2`."
     if padding is None: padding = ks//2
-    return init_default(nn.Conv2d(ni, nf, kernel_size=ks, stride=stride, padding=padding, bias=bias), init)
+    return init_default(nn.Conv2d(ni, nf, kernel_size=ks, stride=stride, 
+                                  padding=padding, bias=bias), init)
 
-def conv2d_trans(ni:int, nf:int, ks:int=2, stride:int=2, padding:int=0, bias=False) -> nn.ConvTranspose2d:
+def conv2d_trans(ni:int, nf:int, ks:int=2, stride:int=2, padding:int=0, bias=False) 
+-> nn.ConvTranspose2d:
     "Create `nn.ConvTranspose2d` layer."
     return nn.ConvTranspose2d(ni, nf, kernel_size=ks, stride=stride, padding=padding, bias=bias)
 
 def relu(inplace:bool=False, leaky:float=None):
     "Return a relu activation, maybe `leaky` and `inplace`."
-    return nn.LeakyReLU(inplace=inplace, negative_slope=leaky) if leaky is not None else nn.ReLU(inplace=inplace)
+    return nn.LeakyReLU(inplace=inplace, negative_slope=leaky) if leaky is not None \
+           else nn.ReLU(inplace=inplace)
 
-def conv_layer(ni:int, nf:int, ks:int=3, stride:int=1, padding:int=None, bias:bool=None, is_1d:bool=False,
-               norm_type:Optional[NormType]=NormType.Batch,  use_activ:bool=True, leaky:float=None,
-               transpose:bool=False, init:Callable=nn.init.kaiming_normal_, self_attention:bool=False):
-    "Create a sequence of convolutional (`ni` to `nf`), ReLU (if `use_activ`) and batchnorm (if `bn`) layers."
+def conv_layer(ni:int, nf:int, ks:int=3, stride:int=1, padding:int=None, bias:bool=None, 
+               is_1d:bool=False, norm_type:Optional[NormType]=NormType.Batch, 
+               use_activ:bool=True, leaky:float=None, transpose:bool=False, 
+               init:Callable=nn.init.kaiming_normal_, self_attention:bool=False):
+    """Create a sequence of convolutional (`ni` to `nf`), ReLU (if `use_activ`) 
+    and batchnorm (if `bn`) layers."""
     if padding is None: padding = (ks-1)//2 if not transpose else 0
     bn = norm_type in (NormType.Batch, NormType.BatchZero)
     if bias is None: bias = not bn
     conv_func = nn.ConvTranspose2d if transpose else nn.Conv1d if is_1d else nn.Conv2d
-    conv = init_default(conv_func(ni, nf, kernel_size=ks, bias=bias, stride=stride, padding=padding), init)
+    conv = init_default(conv_func(ni, nf, kernel_size=ks, bias=bias, 
+                                  stride=stride, padding=padding), init)
     if   norm_type==NormType.Weight:   conv = weight_norm(conv)
     elif norm_type==NormType.Spectral: conv = spectral_norm(conv)
     layers = [conv]
@@ -145,11 +159,13 @@ class SequentialEx(Module):
     def insert(self,i,l): return self.layers.insert(i,l)
 
 class MergeLayer(Module):
-    "Merge a shortcut with the result of the module by adding them or concatenating thme if `dense=True`."
+    """Merge a shortcut with the result of the module by adding them 
+    or concatenating thme if `dense=True`."""
     def __init__(self, dense:bool=False): self.dense=dense
     def forward(self, x): return torch.cat([x,x.orig], dim=1) if self.dense else (x+x.orig)
 
-def res_block(nf, dense:bool=False, norm_type:Optional[NormType]=NormType.Batch, bottle:bool=False, **conv_kwargs):
+def res_block(nf, dense:bool=False, norm_type:Optional[NormType]=NormType.Batch, 
+              bottle:bool=False, **conv_kwargs):
     "Resnet block of `nf` features. `conv_kwargs` are passed to `conv_layer`."
     norm2 = norm_type
     if not dense and (norm_type==NormType.Batch): norm2 = NormType.BatchZero
@@ -169,7 +185,9 @@ class SigmoidRange(Module):
 
 class PartialLayer(Module):
     "Layer that applies `partial(func, **kwargs)`."
-    def __init__(self, func, **kwargs): self.repr,self.func = f'{func}({kwargs})', partial(func, **kwargs)
+    def __init__(self, func, **kwargs):
+           self.repr = f'{func}({kwargs})'
+           self.func = partial(func, **kwargs)
     def forward(self, x): return self.func(x)
     def __repr__(self): return self.repr
 
@@ -200,8 +218,10 @@ def icnr(x, scale=2, init=nn.init.kaiming_normal_):
     x.data.copy_(k)
 
 class PixelShuffle_ICNR(Module):
-    "Upsample by `scale` from `ni` filters to `nf` (default `ni`), using `nn.PixelShuffle`, `icnr` init, and `weight_norm`."
-    def __init__(self, ni:int, nf:int=None, scale:int=2, blur:bool=False, norm_type=NormType.Weight, leaky:float=None):
+    """Upsample by `scale` from `ni` filters to `nf` (default `ni`), 
+    using `nn.PixelShuffle`, `icnr` init, and `weight_norm`."""
+    def __init__(self, ni:int, nf:int=None, scale:int=2, blur:bool=False, 
+                 norm_type=NormType.Weight, leaky:float=None):
         nf = ifnone(nf, ni)
         self.conv = conv_layer(ni, nf*(scale**2), ks=1, norm_type=norm_type, use_activ=False)
         icnr(self.conv[0].weight)
@@ -248,15 +268,18 @@ def CrossEntropyFlat(*args, axis:int=-1, **kwargs):
 
 def BCEWithLogitsFlat(*args, axis:int=-1, floatify:bool=True, **kwargs):
     "Same as `nn.BCEWithLogitsLoss`, but flattens input and target."
-    return FlattenedLoss(nn.BCEWithLogitsLoss, *args, axis=axis, floatify=floatify, is_2d=False, **kwargs)
+    return FlattenedLoss(nn.BCEWithLogitsLoss, *args, axis=axis, 
+                         floatify=floatify, is_2d=False, **kwargs)
 
 def BCEFlat(*args, axis:int=-1, floatify:bool=True, **kwargs):
     "Same as `nn.BCELoss`, but flattens input and target."
-    return FlattenedLoss(nn.BCELoss, *args, axis=axis, floatify=floatify, is_2d=False, **kwargs)
+    return FlattenedLoss(nn.BCELoss, *args, axis=axis, floatify=floatify, 
+                         is_2d=False, **kwargs)
 
 def MSELossFlat(*args, axis:int=-1, floatify:bool=True, **kwargs):
     "Same as `nn.MSELoss`, but flattens input and target."
-    return FlattenedLoss(nn.MSELoss, *args, axis=axis, floatify=floatify, is_2d=False, **kwargs)
+    return FlattenedLoss(nn.MSELoss, *args, axis=axis, floatify=floatify, 
+                         is_2d=False, **kwargs)
 
 class NoopLoss(Module):
     "Just returns the mean of the `output`."
@@ -268,12 +291,14 @@ class WassersteinLoss(Module):
 
 def simple_cnn(actns:Collection[int], kernel_szs:Collection[int]=None,
                strides:Collection[int]=None, bn=False) -> nn.Sequential:
-    "CNN with `conv_layer` defined by `actns`, `kernel_szs` and `strides`, plus batchnorm if `bn`."
+    """CNN with `conv_layer` defined by `actns`, `kernel_szs` and `strides`, 
+    plus batchnorm if `bn`."""
     nl = len(actns)-1
     kernel_szs = ifnone(kernel_szs, [3]*nl)
     strides    = ifnone(strides   , [2]*nl)
     layers = [conv_layer(actns[i], actns[i+1], kernel_szs[i], stride=strides[i],
-              norm_type=(NormType.Batch if bn and i<(len(strides)-1) else None)) for i in range_of(strides)]
+              norm_type=(NormType.Batch if bn and i<(len(strides)-1) else None)) \
+              for i in range_of(strides)]
     layers.append(PoolFlatten())
     return nn.Sequential(*layers)
 
